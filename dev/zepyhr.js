e52ae541d4ad9214ad4af21c2ef206a0f0789553
OttCS <ottcs20@gmail.com>
2022-05-25 01:07:55 -0400

    Mouse Partially Implemented

--- dev/zepyhr.js
+++ dev/zepyhr.js
@@ -4,17 +4,31 @@ PIXI.zephyr = "ZephyrJS version 22.5.23";
 
 PIXI.input = {
     keyMap: new Map(),
-
     // getKeyFired returns true the first time it is called for a key while the key is down
     getKeyFired: (keyStr) => {
         let r = PIXI.input.keyMap.get(keyStr);
         PIXI.input.keyMap.set(keyStr, false);
         return r;
     },
-
     // getKeyDown returns true always if the key is down
     getKeyDown: (keyStr) => {
         return PIXI.input.keyMap.has(keyStr);
+    },
+
+    mouseMap: new Map(),
+    getMouseFired: (btnStr) => {
+        let r = PIXI.input.mouseMap.get(btnStr);
+        PIXI.input.keyMap.set(btnStr, false);
+        return r;
+    },
+    getMouseDown: (btnStr) => {
+        return PIXI.input.mouseMap.has(btnStr);
+    },
+    getMouseX: () => {
+        return PIXI.input.mouseMap.get('x');
+    },
+    getMouseY: () => {
+        return PIXI.input.mouseMap.get('y');
     },
 }
 
@@ -30,22 +44,38 @@ PIXI.collision = {
 }
 
 // https://www.w3schools.com/jsref/met_element_requestfullscreen.asp
-PIXI.utils.openFullScreen = (view) => { // w3schools <3
-    if (view.requestFullscreen) {
-        view.requestFullscreen();
-    } else if (view.webkitRequestFullscreen) { /* Safari */
-        view.webkitRequestFullscreen();
-    } else if (view.msRequestFullscreen) { /* IE11 */
-        view.msRequestFullscreen();
-    }
+PIXI.utils.openFullScreen = (view) => {
+    if (view.requestFullscreen)
+        view.requestFullscreen(); // Standard
+    else if (view.webkitRequestFullscreen)
+        view.webkitRequestFullscreen(); // Safari
+    else if (view.msRequestFullscreen)
+        view.msRequestFullscreen(); // IE11
 }
 
 // Keyboard
 window.addEventListener('keydown', (e) => {
-    PIXI.input.keyMap.set(e.key.toUpperCase(), true);
+    PIXI.input.keyMap.set(e.key.toLowerCase(), true);
 });
 window.addEventListener('keyup', (e) => {
-    PIXI.input.keyMap.delete(e.key.toUpperCase())
+    PIXI.input.keyMap.delete(e.key.toLowerCase());
 });
+
+// Mouse
+window.addEventListener('mousemove', (e) => {
+    PIXI.input.mouseMap.set('x', e.x / window.innerWidth);
+    PIXI.input.mouseMap.set('y', e.y / window.innerHeight);
+});
+window.addEventListener('mousedown', (e) => {
+    PIXI.input.mouseMap.set(e.button, true);
+});
+window.addEventListener('mouseup', (e) => {
+    PIXI.input.mouseMap.delete(e.button);
+});
+
+// Catches/Blocks right click
+window.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+})
 
 console.log(PIXI.zephyr + " is extending Pixi!");
