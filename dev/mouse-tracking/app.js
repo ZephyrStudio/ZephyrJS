592a6f7bf3e4ce96ad0074cae150b3227875de7f
OttCS <ottcs20@gmail.com>
2022-05-25 18:40:13 -0400

    Update app.js

--- dev/mouse-tracking/app.js
+++ dev/mouse-tracking/app.js
@@ -15,8 +15,6 @@ cursor.y = 0;
 cursor.anchor = {x: 0.5, y: 0.5};
 scene.addChild(cursor);
 
-console.log(document.getElementsByTagName("html")[0].getBoundingClientRect());
-
 app.ticker.add((deltaTime) => {
     cursor.x = (PIXI.input.getMouseX() * app.view.width + 0.5) ^ 0;
     cursor.y = (PIXI.input.getMouseY() * app.view.height + 0.5) ^ 0;
