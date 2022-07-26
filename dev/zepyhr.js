d34a7cbab416c573226602d3079122e56cc1f854
OttCS <ottcs20@gmail.com>
2022-07-26 02:14:42 -0400

    22.7.26 #16
    Input Tracker has been fixed to be accurate AND much faster

--- dev/zepyhr.js
+++ dev/zepyhr.js
@@ -1,29 +1,41 @@
 "use strict"
 // ZEPHYR.js directly builds on top of PixiJS for ease of use
-PIXI.zephyr = "ZephyrJS version 22.7.14";
+PIXI.zephyr = "ZephyrJS version 22.7.26";
 
 PIXI.input = {
     keyMap: new Map(),
     // getKeyFired returns true the first time it is called for a key while the key is down
     getKeyFired: (keyStr) => {
-        let r = PIXI.input.keyMap.get(keyStr);
-        PIXI.input.keyMap.set(keyStr, false);
-        return r;
+        if (PIXI.input.keyMap.size > 0 && PIXI.input.keyMap.get(keyStr)) {
+            PIXI.input.keyMap.set(keyStr, false);
+            return true;
+        }
+        return false;
     },
     // getKeyDown returns true always if the key is down
     getKeyDown: (keyStr) => {
-        return PIXI.input.keyMap.has(keyStr);
+        if (PIXI.input.keyMap.size > 0 && PIXI.input.keyMap.has(keyStr)) {
+            PIXI.input.keyMap.set(keyStr, false);
+            return true;
+        }
+        return false;
     },
 
     mouseContainer: document.getElementsByTagName("html")[0],
     mouseMap: new Map(),
     getMouseFired: (btn) => {
-        let r = PIXI.input.mouseMap.get(btn);
-        PIXI.input.keyMap.set(btn, false);
-        return r;
+        if (PIXI.input.mouseMap.size > 0 && PIXI.input.mouseMap.get(btn)) {
+            PIXI.input.mouseMap.set(btn, false);
+            return true;
+        }
+        return false;
     },
     getMouseDown: (btn) => {
-        return PIXI.input.mouseMap.has(btn);
+        if (PIXI.input.mouseMap.size > 0 && PIXI.input.mouseMap.has(btn)) {
+            PIXI.input.mouseMap.set(btn, false);
+            return true;
+        }
+        return false;
     },
     getMouseX: () => {
         return PIXI.input.mouseMap.get('x');
@@ -59,47 +71,11 @@ PIXI.utils.openFullScreen = (view) => {
         view.msRequestFullscreen(); // IE11
 }
 
-PIXI.utils.saveObjectToFile = (name, state) => {
-    let blobState = new Blob([JSON.stringify(state)], {type: 'text/plain'});
-    let a = document.createElement('a');
-    a.download = name + ".json";
-    a.href = window.URL.createObjectURL(blobState);
-    a.click();
-};
-
-PIXI.utils.readObjectFromFile = () => {
-    return new Promise(resolve => {
-        let input = document.createElement('input');
-        input.type = 'file';
-        input.accept = '.json';
-
-        document.body.onfocus = () => {
-            if (input) {
-                let res = null;
-                if(input.target) {
-                    console.log("Files picked");
-                    let file = input.target.files[0]; 
-                    let reader = new FileReader();
-                    reader.readAsText(file,'UTF-8');
-                    reader.onload = readerEvent => {
-                        res = JSON.parse(readerEvent.target.result);
-                    }
-                } else {
-                    console.log("No files picked");
-                }
-                input = null;
-                resolve(res);
-            }
-        }
-
-        input.click();
-    });
-};
-
 // Keyboard
 window.addEventListener('keydown', (e) => {
     PIXI.input.keyMap.set(e.key.toLowerCase(), true);
 });
+
 window.addEventListener('keyup', (e) => {
     PIXI.input.keyMap.delete(e.key.toLowerCase());
 });
